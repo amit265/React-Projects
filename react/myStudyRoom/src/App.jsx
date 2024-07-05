541fa9e2dfbc51a331a5e8a5f4ba15e53ad386a9
Amit kumar <amitk.kumar414@gmail.com>
2024-07-05 14:08:43 +0530

    react folder added

---
+++ react/myStudyRoom/src/App.jsx
@@ -0,0 +1,17 @@
+import './index.css'
+
+function App() {
+
+  return (
+    <>
+     <h1 className="text-5xl font-bold underline text-center text-red-900">
+      Hello world!
+    </h1>
+    <div>
+      
+    </div>
+    </>
+  )
+}
+
+export default App
