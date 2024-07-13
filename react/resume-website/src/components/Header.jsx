f773c83e28b13d68ef6cba3447a22dd3026e4b8d
Amit kumar <amitk.kumar414@gmail.com>
2024-07-13 23:12:02 +0530

    changes in resume website

--- react/resume-website/src/components/Header.jsx
+++ react/resume-website/src/components/Header.jsx
@@ -4,7 +4,7 @@ function Header() {
   return (
     <header className="bg-blue-600 text-white p-4 text-center">
       <h1 className="text-4xl font-bold">Amit Kumar</h1>
-      <p className="text-xl">Aspiring Full Stack Developer</p>
+      <p className="text-xl"></p>
     </header>
   );
 }
